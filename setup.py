c43997d6f45e8929cfaa1391326362a00e39c38c
Sayo <ws11579@gmail.com>
2025-05-08 00:03:56 +0800

    edit readme

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="ace_step",
     description="ACE Step: A Step Towards Music Generation Foundation Model",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     version="0.1.0",
     packages=["acestep"],
-    install_requires=open("requirements.txt").read().splitlines(),
+    install_requires=open("requirements.txt", encoding="utf-8").read().splitlines(),
     author="ACE Studio, StepFun AI",
     license="Apache 2.0",
     classifiers=[
